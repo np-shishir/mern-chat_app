700e57c016611df47b7348e1945740b810246192
np-shishir <shishir.nepal404@gmail.com>
2025-10-19 08:14:24 +0545

    login, logout, signup controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { login, logout, signup, updateProfile } from '../controllers/auth.controller.js'
+import { protectRoute } from '../middlewares/auth.middleware.js'
 const router = express.Router()
 
 router.post("/signup", signup)
