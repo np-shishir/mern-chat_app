66a1be6539deab47a3b1e463b6edd9640c280d02
np-shishir <shishir.nepal404@gmail.com>
2025-10-27 14:00:19 +0545

    updateProfile controller and message model + backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import authRoute from './routes/auth.route.js'
+import messageRoute from './routes/message.route.js'
+import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js'
 const app = express()
 dotenv.config()
 app.use(express.json())
 app.use("/api/auth", authRoute)
-
+app.use("/api/message", messageRoute)
+app.use(cookieParser)
 
 
 const PORT = process.env.PORT || 5001
